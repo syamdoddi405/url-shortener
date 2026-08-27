28b556ea3abaeb64193c8d102c71f6d8f068016c
syamdoddi405 <55868880+syamdoddi405@users.noreply.github.com>
2026-08-27 09:53:29 +0530

    Refactor: Update repositories to use correct entity classes

--- src/main/java/com/url/shortener/repository/AnalyticsRepository.java
+++ src/main/java/com/url/shortener/repository/AnalyticsRepository.java
@@ -1,15 +1,22 @@
 package com.url.shortener.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.url.shortener.entity.AnalyticsEntity;
 
-import com.url.shortener.dto.AnalyticsDTO;
-
+/**
+ * Repository for AnalyticsEntity persistence.
+ * Updated to use the correct entity class (AnalyticsEntity).
+ */
 @Repository
-public interface AnalyticsRepository extends JpaRepository<AnalyticsDTO, Long> {
-	
-    Optional<AnalyticsDTO> findByShortCode(String shortCode);
-
+public interface AnalyticsRepository extends JpaRepository<AnalyticsEntity, Long> {
+    
+    /**
+     * Find analytics data by short code.
+     *
+     * @param shortCode the short code
+     * @return Optional containing the analytics entity if found
+     */
+    Optional<AnalyticsEntity> findByShortCode(String shortCode);
 }
